1d46ac9622b7e94ca937a191b0f6fdea391ce77c
Ashish Negi <ashishnegi33@gmail.com>
2017-10-31 08:24:06 +0530

    Completes fileapis and testing in main.

--- src/fileapi.rs
+++ src/fileapi.rs
@@ -1,6 +1,16 @@
+use std::fs;
 use std::fs::File;
 use std::io::Error;
 use std::io::prelude::Write;
+use std::io::prelude::Read;
+use std::fs::OpenOptions;
+
+// We give apis :
+// create_key : creates a persisted key with value.
+// get_value : get value for a key.
+// put_value : change value for a key.
+// key_exists : check if key exists.
+// delete_key : deletes the key.
 
 pub fn create_key(key : &str, value : &[u8]) -> Result<(), Error> {
     // How to atomically create a file if it does not exist ?
@@ -13,3 +23,25 @@ pub fn create_key(key : &str, value : &[u8]) -> Result<(), Error> {
     buffer.write_all(value)
 }
 
+pub fn get_value(key : &str) -> Result<Vec<u8>, Error> {
+    let mut file = File::open(key)?;
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)?;
+    Ok(contents)
+}
+
+pub fn put_value(key : &str, value: &[u8]) -> Result<(), Error> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(key)?;
+    file.write_all(value)
+}
+
+pub fn key_exists(key : &str) -> bool {
+    fs::metadata(key).is_ok()
+}
+
+pub fn delete_key(key: &str) -> Result<(), Error> {
+    fs::remove_file(key)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 mod fileapi;
 
 fn main() {
-    println!("Hello, world!");
-    assert!(fileapi::create_key("hello", b"world").is_ok());
+    let key = "hello";
+    assert!(fileapi::create_key(key, b"world").is_ok());
+    assert!(fileapi::key_exists(key));
+
+    {
+        let value = String::from("world").into_bytes();
+        assert!(match fileapi::get_value(key) {
+            Ok(val) => val == value,
+            _ => false
+        });
+    }
+
+    assert!(fileapi::put_value(key, b"world2").is_ok());
+    {
+        let value2 = String::from("world2").into_bytes();
+        assert!(match fileapi::get_value(key) {
+            Ok(val) => val == value2,
+            _ => false
+        });
+    }
+    assert!(fileapi::delete_key(key).is_ok());
 }
