87d940ca2e275a1effc9d3ae9e06810440e5eb31
Ashish Negi <ashishnegi33@gmail.com>
2017-11-01 14:11:27 +0530

    Implement drop trait for list to not blow stack while getting destructed.

--- src/list.rs
+++ src/list.rs
@@ -42,6 +42,22 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut head = mem::replace(&mut self.head, Link::Nil);
+
+        while true {
+            match mem::replace(&mut head, Link::Nil) {
+                Link::Nil => break,
+                Link::More(ref mut node) => {
+                    head = mem::replace(&mut node.next, Link::Nil);
+                }
+            }
+        }
+
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -61,5 +77,13 @@ mod test {
         assert_eq!(Some(2), list.remove());
         assert_eq!(Some(1), list.remove());
         assert_eq!(None, list.remove());
+    }
+
+    #[test]
+    fn drop_long_list() {
+        let mut list = List::new();
+        for i in 1..100000 {
+            list.insert(i);
+        }
     }
 }
