0585d4c461e8f1c03b7481fa84432a5ed9228dc5
Ashish Negi <ashishnegi33@gmail.com>
2017-11-10 00:38:46 +0530

    Failing to compile multi thread code.

--- src/consistency.rs
+++ src/consistency.rs
@@ -104,8 +104,8 @@ mod test {
         }
 
         fn put_value(&mut self, key : &str, value: &[u8]) -> Result<(), Error> {
-            self.data.insert(key.to_string(), Vec::from(value)).map_or(Err(Error::new(ErrorKind::Other, "Failed to write")),
-                |v| Ok(()))
+            self.data.insert(key.to_string(), Vec::from(value));
+            Ok(())
         }
 
         fn key_exists(&self, key : &str) -> bool {
@@ -116,5 +116,52 @@ mod test {
             self.data.remove(key).map_or(Err(Error::new(ErrorKind::NotFound, "Key does not exists")),
                 |v| Ok(()))
         }
+    }
+
+    extern crate rand;
+    use self::rand::Rng;
+    use std::thread;
+    use std::sync::Arc;
+
+    #[test]
+    fn multiple_threads_bombing_storage() {
+        const parallels : usize = 10;
+        // let mut storage = MemoryStorage { data: HashMap::new() };
+        // let mut consistency = Consistency::new(&mut storage);
+
+        // create all keys.
+        let mut keys = Vec::<String>::new();
+        for k in 1..parallels {
+            keys.push(k.to_string());
+        }
+
+        let zero = "0".as_bytes();
+        // put default values in files.
+        for key in keys.clone() {
+            // consistency.put_value(&key, zero).unwrap();
+        }
+
+        // test write threads
+        let write_thread = |to_write: Vec<u8>, keys: &mut Vec<String>| {
+            let mut rng = rand::thread_rng();
+            rng.shuffle(keys);
+
+            for key in keys {
+                // let mut val = consistency.get_value(key).unwrap();
+                // val.append(to_write.clone().as_mut());
+                // consistency.put_value(key, &val).unwrap();
+            }
+        };
+
+        // start parallels threads..
+        // for t in 1..parallels {
+        //     let handle = thread::spawn(move || {
+        //         write_thread(t.to_string().into_bytes(), &mut keys);
+        //     });
+        // }
+
+        // wait for all threads..
+
+        // check if all parallels keys have all numbers in any order.
     }
 }
